f5587e76949e2550a95b43fa96fda65bf1c1dbf9
Muhammad Noshad <muhammadnoshad137@gmail.com>
2024-03-31 15:01:55 +0500

    App done

--- src/MainBody.js
+++ src/MainBody.js
@@ -1,13 +1,54 @@
+import { useState } from "react";
 import QuoteSection from "./QuoteSection";
 
 const MainBody = () => {
+  const [quote, setQuote] = useState(null);
+  const [isFetched, setIsFetched] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [animation, setAnimation] = useState("");
+  const [isFailed, setIsFailed] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  function getQuote(){
+    setQuote("null");
+    setIsProcessing(true);
+    setAnimation("glow-animation");
+    setIsFailed(false);
+    setIsFetched(false);
+    setIsClicked(true);
+
+    let options = {
+      method: 'GET',
+      headers: { 'x-api-key': 'RilDBSInYEk65jdV8GgVCQ==QUrS7GOd9U3xHpyV' }
+    }
+    
+    let url = 'https://api.api-ninjas.com/v1/quotes'
+    
+    
+    fetch(url,options)
+          .then(res => res.json())
+          .then(data => {
+            setQuote(data[0]);
+            setIsFetched(true);
+            setIsProcessing(false);
+            setAnimation("");
+          })
+          .catch(err => {
+              console.log(`error ${err}`);
+              setIsFailed(true);
+              setIsFetched(false);
+              setIsProcessing(false);
+              setAnimation("error-state");
+          }); 
+  }
+
   return ( 
-    <div className="main-body">
+    <div className={ "main-body" + " " + animation}>
       <p className="title">
         RandQuoto
       </p>
-      <QuoteSection />
-      <button className="generate-btn">Generate</button>
+      <QuoteSection quote={ quote } isFetched = { isFetched } isProcessing={ isProcessing } isFailed={ isFailed } isClicked={ isClicked } />
+      <button className="generate-btn" onClick={() => { getQuote(); }}>Generate</button>
     </div>
   );
 }
--- src/QuoteSection.js
+++ src/QuoteSection.js
@@ -1,9 +1,34 @@
-const QuoteSection = () => {
+const QuoteSection = ({ quote, isFetched, isProcessing, isFailed, isClicked }) => {
+  let timeoutId = null;
+  
+  function copyQuote(){
+    isFetched &&
+    navigator.clipboard.writeText(quote.quote + "\n- " + quote.author);
+  }
+
+  function showCopiedMessage(){
+    if(isFetched){
+      const copiedElement = document.body.querySelector('.copied');
+      
+      copiedElement.classList.add('copied-show');
+
+      timeoutId && clearTimeout(timeoutId);
+
+      timeoutId = setTimeout(() => {
+        copiedElement.classList.remove('copied-show')
+      }, 1000);
+    }
+  }
+  
   return ( 
     <div className="quote-section">
-      <p className="quote">
-      Whoever fights monsters should see to it that in the process he does not become a monster. And if you gaze long enough into an abyss, the abyss will gaze back into you.
-      </p>
+      { !isFetched && !isClicked && <p>Click 'Generate' to generate a random quote.</p>}
+      { isProcessing && <p>Hang tight...</p> }
+      { isFailed && <p>An error occurred!</p> }
+      { isFetched && <p> { quote.quote } <br /> <br /> {"-  " + quote.author } </p> }
+      <i className="material-symbols-outlined" onClick={ () => { copyQuote(); showCopiedMessage(); }}>content_copy</i>
+      <span className="tooltip">Copy</span>
+      <span className="copied">Copied</span>
     </div>
   );
 }
